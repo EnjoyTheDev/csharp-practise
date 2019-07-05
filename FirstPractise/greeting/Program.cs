3ba0d7b907c4b216f4da418e2bf04148a33fd784
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-07-05 09:02:24 +0300

    Final

--- FirstPractise/greeting/Program.cs
+++ FirstPractise/greeting/Program.cs
@@ -6,24 +6,25 @@ namespace greeting
   {
     static void Main(string[] args)
     {
-      TimeSpan time = DateTime.Now.TimeOfDay;
+      var time = DateTime.Now.TimeOfDay;
+      var timeNow = DateTime.Now;
 
       if (time.Hours >= 9 && time.Hours <= 12)
       {
-        Console.WriteLine("Good mogning, guys");
+        Console.WriteLine($"{timeNow} Good mogning, guys");
       }
       else if (time.Hours >= 12 && time.Hours <= 15)
       {
-        Console.WriteLine("Good day, guys");
+        Console.WriteLine($"{timeNow} Good day, guys");
       }
       else if (time.Hours >= 15 && time.Hours <= 22)
       {
-        Console.WriteLine("Good evening, guys");
+        Console.WriteLine($"{timeNow} Good evening, guys");
       }
       else
       {
         return;
       }
-    }
+     }
   }
 }
