36c5c7c03a8d22848ef45b20655d361dbd33beef
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-08-01 21:22:34 +0300

    lecture_12

---
+++ FirstPractise/lecture_12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace lecture_12
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      CreatArray();
+      Console.ReadLine();
+    }
+    struct Reactangle
+    {
+      public int width;
+      public int heigth;
+    }
+    static void CreatArray()
+    {
+      Random rand = new Random();
+      int min = 1;
+      int max = 10;
+      Reactangle[] arrayRectangle = new Reactangle[100];
+      for (int i = 0; i < arrayRectangle.Length; i++)
+      {
+        arrayRectangle[i].heigth = rand.Next(min, max);
+        arrayRectangle[i].width = rand.Next(min, max);
+        Console.Write($"\n Heigth: {arrayRectangle[i].heigth}, Width : {arrayRectangle[i].width}");
+      }
+      var equalNumbers = 0;
+      for (int i = 1; i < arrayRectangle.Length; i++)
+      {
+        if (arrayRectangle[i].Equals(arrayRectangle[0]))
+        {
+          equalNumbers++;
+        }
+      }
+      Console.WriteLine($"\n\t Equals: {equalNumbers}");
+    }
+  }
+}
+
+
