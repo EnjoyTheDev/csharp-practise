ec400aeed8ec3459210509a06c943b9e87454974
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-08-15 00:01:29 +0300

    lecture_13

---
+++ FirstPractise/lecture_13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace lecture_13
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Shape[] shapeArray = new Shape[3];
+
+      shapeArray[0] = new CircleArea();
+      shapeArray[1] = new SquareArea();
+      shapeArray[2] = new TriangleArea();
+
+      for (int i = 0; i < shapeArray.Length; i++)
+      {
+        switch (shapeArray[i])
+        {
+          case CircleArea circle:
+            {
+              circle.CalcArea();
+              Console.WriteLine($"\n This is {circle.ShapeName}. CLR Type is {circle.GetType().Name}. Square is {circle.circleArea}.");
+              break;
+            }
+          case SquareArea square:
+            {
+              square.CalcArea();
+              Console.WriteLine($"\n This is {square.ShapeName}. CLR Type is {square.GetType().Name}. Square is {square.squareArea}.");
+              break;
+            }
+
+          case TriangleArea triangle:
+            {
+              triangle.CalcArea();
+              Console.WriteLine($"\n This is {triangle.ShapeName}. CLR Type is {triangle.GetType().Name}. Square is {triangle.triangleArea}.");
+              break;
+            }
+        }
+      }
+    }
+  }
+}
---
+++ FirstPractise/lecture_13/Shape.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lecture_13
+{
+  internal class Shape
+  {
+    internal int oneSide = 5;
+
+    public virtual void CalcArea()
+    {
+      Console.WriteLine("Area");
+    }
+  }
+
+  internal class CircleArea : Shape
+  {
+    public string ShapeName { get; } = "Circle";
+    public double circleArea { get; set; }
+
+    public override void CalcArea()
+    {
+      int radius = 4;
+      circleArea = Math.Round(Math.PI * Math.Pow(radius, 2), 2);
+    }
+  }
+
+  internal class SquareArea : Shape
+  {
+    internal string ShapeName { get; } = "Square";
+    public double squareArea { get; set; }
+
+    public override void CalcArea()
+    {
+      squareArea = Math.Pow(oneSide, 2);
+    }
+  }
+
+  internal class TriangleArea : Shape
+  {
+    internal string ShapeName { get; } = "Triangle";
+    public double triangleArea { get; set; }
+
+    public override void CalcArea()
+    {
+      double height = 9;
+      triangleArea = (height * oneSide / 2);
+    }
+  }
+}
