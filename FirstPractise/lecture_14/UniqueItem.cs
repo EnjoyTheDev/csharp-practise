0b028eb9b74ff678dd55640155a2abc4c0eecbe0
EnjoyTheDev <EnjoyTheDev@users.noreply.github.com>
2019-08-15 21:05:41 +0300

    lecture_14

---
+++ FirstPractise/lecture_14/UniqueItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace lecture_14
+{
+  public class UniqueItem
+  {
+    public readonly int Id = 3;
+    private static int InstanceCounter;
+
+    public UniqueItem()
+    {
+      Id = Id + InstanceCounter;
+      ++InstanceCounter;
+    }
+
+    public int UniqueId
+    {
+      get { return Id; }
+    }
+  }
+}
